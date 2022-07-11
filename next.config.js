969a888dae4dde397222de0ffa7fb99d394df661
huffmanks <55319887+huffmanks@users.noreply.github.com>
2022-07-11 16:31:21 -0400

    next config not working

--- next.config.js
+++ next.config.js
@@ -2,12 +2,28 @@
 const withPWA = require('next-pwa')
 
 module.exports = (phase, { defaultConfig }) => {
-    if (phase === 'phase-development-server') {
+    const devServerPhase = phase === 'phase-development-server' ?? false
+    const exportPhase = phase === 'phase-export' ?? false
+    const STAGING = process.env.STAGING
+    if (devServerPhase) {
         return {
             reactStrictMode: true,
             swcMinify: true,
         }
     } else {
+        console.log('phase__', exportPhase)
+        console.log('env__', STAGING)
+        async function customPaths(defaultPathMap, {}) {
+            return {
+                '/': { page: '/' },
+                '/aspect-ratio/index': { page: '/aspect-ratio' },
+                '/color-picker/index': { page: '/color-picker' },
+                '/email-signature/index': { page: '/email-signature' },
+                '/password-generator/index': { page: '/password-generator' },
+                '/text-formatter/index': { page: '/text-formatter' },
+                '/unit-converter/index': { page: '/unit-converter' },
+            }
+        }
         return withPWA({
             pwa: {
                 dest: 'public',
@@ -15,11 +31,13 @@ module.exports = (phase, { defaultConfig }) => {
             },
             reactStrictMode: true,
             swcMinify: true,
+            assetPrefix: exportPhase && STAGING ? '.' : '',
             experimental: {
                 images: {
                     unoptimized: true,
                 },
             },
+            exportPathMap: exportPhase && STAGING ? customPaths() : null,
         })
     }
 }
