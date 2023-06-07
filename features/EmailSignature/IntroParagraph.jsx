ed3d0809908736383a1fa17f97a37394312a593a
huffmanks <huffmanks2@gmail.com>
2023-06-07 15:07:05 -0400

    reduced intro for email sign and updated todo

--- features/EmailSignature/IntroParagraph.jsx
+++ features/EmailSignature/IntroParagraph.jsx
@@ -3,13 +3,8 @@ import { Typography } from '@mui/material'
 const IntroParagraph = () => {
     return (
         <>
-            <Typography paragraph>
-                Each email is an opportunity to represent yourself positively and professionally with a consistent message and design that contribute to improved awareness and branding.
-            </Typography>
-            <Typography paragraph>In less than two minutes, you can create a personalized signature to use on mobile devices and desktops.</Typography>
             <Typography paragraph mb={5}>
-                Please note that installing or updating your signature on one device does not affect your email signatures on other devices. Your signature will need to be added or updated on each
-                device from which you wish to use it.
+                In less than two minutes, you can create a personalized signature to use on mobile devices and desktops.
             </Typography>
         </>
     )
--- todo.md
+++ todo.md
@@ -8,9 +8,11 @@
 2. Item Picker
 
     - [ ] Ouput to comma, pipe
+    - [ ] Upload add json, csv options
 
-3. Todo List
-    - [ ] Edit
-        - [ ] rearrangeable list
-    - [ ] View
-        - [ ] sorts lists by title, updated, created, type and favorites
+3. New tools
+
+    - [ ] Image resizer using sharpjs. Take a photo and generate hero, thumb, etc.: https://sharp.pixelplumbing.com
+    - [ ] Dummy HTML generator, i.e.: https://www.htmlstrip.com/lorem-ipsum-generator but use ipsum alternative like https://www.dummytextgenerator.com
+    - [ ] Compare text, i.e.: https://www.diffchecker.com
+    - [ ] Random keygen, i.e.: https://randomkeygen.com
