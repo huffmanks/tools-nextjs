6d30cc0ff90330ea00cc853f88b2790c64a5fc0f
huffmanks <huffmanks2@gmail.com>
2023-06-12 18:22:40 -0400

    seo meta tags added name property in conjuction with property

--- components/common/SEO.jsx
+++ components/common/SEO.jsx
@@ -9,18 +9,19 @@ const SEO = ({ description, title, url, imageUrl }) => {
             <meta name='title' content={title} />
             <meta name='description' content={description} />
 
-            <meta property='og:type' content='website' />
-            <meta property='og:title' content={title} />
-            <meta property='og:url' content={`${baseUrl}${url}`} />
-            <meta property='og:description' content={description} />
-            <meta property='og:site_name' content='Stratools' />
-            <meta property='og:image' content={imageUrl ? `${baseUrl}/previews${imageUrl}` : `${baseUrl}/logos/stratools-stacked.png`} />
+            <meta name='og:type' property='og:type' content='website' />
+            <meta name='og:title' property='og:title' content={title} />
+            <meta name='og:url' property='og:url' content={`${baseUrl}${url}`} />
+            <meta name='og:description' property='og:description' content={description} />
+            <meta name='og:site_name' property='og:site_name' content='Stratools' />
+            <meta name='og:image' property='og:image' content={imageUrl ? `${baseUrl}/previews${imageUrl}` : `${baseUrl}/logos/stratools-stacked.png`} />
 
-            <meta property='twitter:card' content='summary_large_image' />
-            <meta property='twitter:title' content={title} />
-            <meta property='twitter:url' content={`${baseUrl}${url}`} />
-            <meta property='twitter:description' content={description} />
-            <meta property='twitter:image' content={imageUrl ? `${baseUrl}/previews${imageUrl}` : `${baseUrl}/logos/stratools-stacked.png`} />
+            <meta name='twitter:card' property='twitter:card' content='summary_large_image' />
+            <meta name='twitter:domain' property='twitter:domain' content='tools.huffmanks.com' />
+            <meta name='twitter:title' property='twitter:title' content={title} />
+            <meta name='twitter:url' property='twitter:url' content={`${baseUrl}${url}`} />
+            <meta name='twitter:description' property='twitter:description' content={description} />
+            <meta name='twitter:image' property='twitter:image' content={imageUrl ? `${baseUrl}/previews${imageUrl}` : `${baseUrl}/logos/stratools-stacked.png`} />
         </Head>
     )
 }
