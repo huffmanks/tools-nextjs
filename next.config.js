59018bc5bb0036dbdf2400204aa777db8bb482bc
Kevin Huffman <huffmanks2@gmail.com>
2026-06-20 14:47:29 -0400

    fix sw and offline

--- next.config.js
+++ next.config.js
@@ -1,45 +1,39 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
+const isDev = process.env.NODE_ENV === "development";
+const isStagingExport = process.env.STAGING === "true";
+const calculatedBasePath = isStagingExport ? "/huffmanks/out" : "";
 
-module.exports = (phase, { defaultConfig }) => {
-    if (phase === 'phase-development-server') {
-        return {
-            reactStrictMode: true,
-            swcMinify: true,
-        }
-    } else {
-        return withPWA({
-            pwa: {
-                dest: 'public',
-                swSrc: 'service-worker.js',
-            },
-            reactStrictMode: true,
-            swcMinify: true,
-            basePath: phase === 'phase-export' && process.env.STAGING === 'true' ? '/huffmanks/out' : '',
-            // experimental: {
-            //     images: {
-            //         unoptimized: true,
-            //     },
-            // },
-            ...(phase === 'phase-export' &&
-                process.env.STAGING === 'true' && {
-                    exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-                        return {
-                            '/': { page: '/' },
-                            '/calculate/aspect-ratio/index': { page: '/calculate/aspect-ratio' },
-                            '/calculate/units/index': { page: '/calculate/units' },
-                            '/format/text/index': { page: '/format/text' },
-                            '/generate/css-scrollbar/index': { page: '/generate/css-scrollbar' },
-                            '/generate/email-signature/index': { page: '/generate/email-signature' },
-                            '/generate/password/index': { page: '/generate/password' },
-                            '/generate/qr-code/index': { page: '/generate/qr-code' },
-                            '/generate/todo/index': { page: '/generate/todo' },
-                            '/picker/color/index': { page: '/picker/color' },
-                            '/picker/item/index': { page: '/picker/item' },
-                            '/picker/number/index': { page: '/picker/number' },
-                        }
-                    },
-                }),
-        })
-    }
-}
+const withPWA = require("next-pwa")({
+  dest: "public",
+  swSrc: "service-worker.js",
+  disable: false,
+  mode: isDev ? "development" : "production",
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+  basePath: calculatedBasePath,
+  images: {
+    unoptimized: true,
+  },
+  ...(isStagingExport && {
+    exportPathMap: async function () {
+      return {
+        "/": { page: "/" },
+        "/fallback": { page: "/fallback" },
+        "/calculate/aspect-ratio/index": { page: "/calculate/aspect-ratio" },
+        "/calculate/units/index": { page: "/calculate/units" },
+        "/format/text/index": { page: "/format/text" },
+        "/generate/css-scrollbar/index": { page: "/generate/css-scrollbar" },
+        "/generate/email-signature/index": { page: "/generate/email-signature" },
+        "/generate/password/index": { page: "/generate/password" },
+        "/generate/qr-code/index": { page: "/generate/qr-code" },
+        "/generate/todo/index": { page: "/generate/todo" },
+        "/picker/color/index": { page: "/picker/color" },
+        "/picker/item/index": { page: "/picker/item" },
+        "/picker/number/index": { page: "/picker/number" },
+      };
+    },
+  }),
+});
--- service-worker.js
+++ service-worker.js
@@ -1,152 +1,19 @@
-import { skipWaiting, clientsClaim } from 'workbox-core'
-import { ExpirationPlugin } from 'workbox-expiration'
-import { NetworkOnly, NetworkFirst, CacheFirst, StaleWhileRevalidate } from 'workbox-strategies'
-import { registerRoute, setDefaultHandler, setCatchHandler } from 'workbox-routing'
-import { matchPrecache, precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching'
+import { clientsClaim, skipWaiting } from "workbox-core";
+import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
+import { registerRoute, setDefaultHandler } from "workbox-routing";
+import { StaleWhileRevalidate } from "workbox-strategies";
 
-skipWaiting()
-clientsClaim()
+skipWaiting();
+clientsClaim();
 
-const WB_MANIFEST = self.__WB_MANIFEST
+precacheAndRoute(self.__WB_MANIFEST);
+cleanupOutdatedCaches();
 
-WB_MANIFEST.push({
-    url: '/fallback',
-    revision: '1234567890',
-})
-precacheAndRoute(WB_MANIFEST)
+registerRoute(
+  ({ request }) => request.mode === "navigate" || request.destination === "document",
+  new StaleWhileRevalidate({
+    cacheName: "stratools-pages-cache",
+  }),
+);
 
-cleanupOutdatedCaches()
-registerRoute(
-    '/',
-    new StaleWhileRevalidate({
-        cacheName: 'start-url',
-        plugins: [
-            new ExpirationPlugin({
-                maxEntries: 1,
-                maxAgeSeconds: 31536e3,
-                purgeOnQuotaError: !0,
-            }),
-        ],
-    }),
-    'GET'
-)
-registerRoute(
-    /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
-    new CacheFirst({
-        cacheName: 'google-fonts',
-        plugins: [
-            new ExpirationPlugin({
-                maxEntries: 4,
-                maxAgeSeconds: 31536e3,
-                purgeOnQuotaError: !0,
-            }),
-        ],
-    }),
-    'GET'
-)
-registerRoute(
-    /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
-    new CacheFirst({
-        cacheName: 'static-font-assets',
-        plugins: [
-            new ExpirationPlugin({
-                maxEntries: 4,
-                maxAgeSeconds: 31536e3,
-                purgeOnQuotaError: !0,
-            }),
-        ],
-    }),
-    'GET'
-)
-registerRoute(
-    /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-    new StaleWhileRevalidate({
-        cacheName: 'static-image-assets',
-        plugins: [
-            new ExpirationPlugin({
-                maxEntries: 64,
-                maxAgeSeconds: 31536e3,
-                purgeOnQuotaError: !0,
-            }),
-        ],
-    }),
-    'GET'
-)
-registerRoute(
-    /\.(?:js)$/i,
-    new StaleWhileRevalidate({
-        cacheName: 'static-js-assets',
-        plugins: [
-            new ExpirationPlugin({
-                maxEntries: 32,
-                maxAgeSeconds: 31536e3,
-                purgeOnQuotaError: !0,
-            }),
-        ],
-    }),
-    'GET'
-)
-registerRoute(
-    /\.(?:css|less)$/i,
-    new StaleWhileRevalidate({
-        cacheName: 'static-style-assets',
-        plugins: [
-            new ExpirationPlugin({
-                maxEntries: 32,
-                maxAgeSeconds: 31536e3,
-                purgeOnQuotaError: !0,
-            }),
-        ],
-    }),
-    'GET'
-)
-registerRoute(
-    /\.(?:json|xml|csv)$/i,
-    new StaleWhileRevalidate({
-        cacheName: 'static-data-assets',
-        plugins: [
-            new ExpirationPlugin({
-                maxEntries: 32,
-                maxAgeSeconds: 31536e3,
-                purgeOnQuotaError: !0,
-            }),
-        ],
-    }),
-    'GET'
-)
-registerRoute(
-    /.*/i,
-    new StaleWhileRevalidate({
-        cacheName: 'others',
-        networkTimeoutSeconds: 10,
-        plugins: [
-            new ExpirationPlugin({
-                maxEntries: 32,
-                maxAgeSeconds: 31536e3,
-                purgeOnQuotaError: !0,
-            }),
-        ],
-    }),
-    'GET'
-)
-
-setDefaultHandler(new StaleWhileRevalidate())
-
-setCatchHandler(({ event }) => {
-    switch (event.request.destination) {
-        case 'document':
-            return matchPrecache('/fallback')
-            // return caches.match('/fallback')
-            break
-        case 'image':
-            return matchPrecache('/logos/stratools-stacked.png')
-            // return caches.match('/static/images/fallback.png')
-            break
-        // case 'font':
-        // return matchPrecache(FALLBACK_FONT_URL);
-        // return caches.match('/static/fonts/fallback.otf')
-        // break
-        default:
-            return Response.error()
-    }
-})
+setDefaultHandler(new StaleWhileRevalidate());
